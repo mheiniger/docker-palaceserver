71b488429bab1432b25698a0cefca54ba6bad0a3
Michael Heiniger <michael.heiniger@gmail.com>
2022-11-22 00:28:31 +0100

    adding apache and php

---
+++ app/palace/media/_defaultscript.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+</head>
+<body>
+<!-- Rename this script to defaultscript.php to show some infos in the PalaceChat client on login. -->
+<?php
+echo "Current Server News:";
+echo "Its up and running";
+?>
+
+</body>
+</html>
