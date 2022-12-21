84ab78f71af589ea28cf554e3c47fe3cfec97400
Michael Heiniger <michael.heiniger@gmail.com>
2022-12-21 14:35:47 +0100

    fix(startup): fix file copy and logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 
 # initial setup
 if [ -z "$(ls -A /app/run/media)" ]; then
-    cp -r -v /app/palace/media/* /app/run/media/
+    cp -r -v /app/palace/media/. /app/run/media/
     chown -R palace:palace /app/run/media
 fi
 
 if [ -z "$(ls -A /app/run/psdata)" ]; then
-    cp -r -v /app/palace/psdata/* /app/run/psdata/
+    cp -r -v /app/palace/psdata/. /app/run/psdata/
     chown -R palace:palace /app/run/psdata
 fi
 
@@ -19,19 +19,19 @@ if [ -z "$(ls -A /app/run/logs)" ]; then
 fi
 
 if [ -z "$(ls -A /app/run/avatars)" ]; then
-    cp -r -v /app/palace/avatars/* /app/run/avatars/
+    cp -r -v /app/palace/avatars/. /app/run/avatars/
     chown -R palace:palace /app/run/avatars
 fi
 
 if [ -z "$(ls -A /app/run/webservice/props)" ]; then
-    cp -r -v /app/palace/webservice/* /app/run/webservice/
+    cp -r -v /app/palace/webservice/. /app/run/webservice/
     chown -R palace:palace /app/run/webservice
 fi
 
 chown -R palace:palace /app/run
 
 # if [ ! -d "/app/run/logs" ]; then 
-#     cp -r /app/palace/logs/* /app/run/logs/
+#     cp -r /app/palace/logs/. /app/run/logs/
 # fi
 
 su palace
@@ -39,4 +39,4 @@ su palace
 sudo --user=palace /app/bin/pserver -f /app/run/psdata/pserver.conf &
  echo $! > /app/run/logs/pserver.pid
 
-tail -f /app/palace/logs/pserver.log
+tail -f /app/run/logs/pserver.log
